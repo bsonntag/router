1ac1360c71b8ba4e80bb13185c43a19553b21f8c
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-13 23:58:17 +0100

    Improve Route component

--- src/route.js
+++ src/route.js
@@ -1,19 +1,19 @@
 import { MatchContext, matchPath } from './match';
-import React from 'react';
+import React, { useMemo } from 'react';
 import useRouter from './use-router';
 
 const Route = ({ children, partial, path }) => {
   const { location } = useRouter();
-  const match = matchPath(location.pathname, { partial, path });
+  const match = useMemo(() => {
+    return matchPath(location.pathname, { partial, path });
+  }, [location, partial, path]);
 
   if (!match) {
     return null;
   }
 
   return (
-    <MatchContext.Provider value={match}>
-      {children}
-    </MatchContext.Provider>
+    <MatchContext.Provider value={match}>{children}</MatchContext.Provider>
   );
 };
 
