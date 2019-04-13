69d45143e4ce218413e37fd81a6f2f50288b3f69
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-13 23:02:16 +0100

    Add goBack and update navigate with replace

--- example/about.js
+++ example/about.js
@@ -1,5 +1,15 @@
+import { useRouter } from '../src';
 import React from 'react';
 
-const About = () => <h1>About</h1>;
+const About = () => {
+  const { goBack } = useRouter();
+
+  return (
+    <>
+      <h1>About</h1>
+      <button onClick={goBack}>Back</button>
+    </>
+  );
+};
 
 export default About;
--- src/router.js
+++ src/router.js
@@ -1,38 +1,44 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import RouterContext from './router-context';
 
-function isSameLocation(location, otherLocation) {
-  return location.pathname === otherLocation.pathname;
+const getWindowLocation = () => ({
+  hash: window.location.hash,
+  host: window.location.host,
+  hostname: window.location.hostname,
+  href: window.location.href,
+  origin: window.location.origin,
+  pathname: window.location.pathname,
+  port: window.location.port,
+  protocol: window.location.protocol,
+  search: window.location.search,
+  state: window.history.state,
+});
+
+function goBack() {
+  window.history.back();
 }
 
 const Router = ({ children }) => {
-  const [location, setLocation] = useState(() => ({
-    pathname: window.location.pathname,
-  }));
-
-  const contextValue = useMemo(
-    () => ({
-      location,
-      navigate: location => setLocation(location),
-    }),
-    [location]
-  );
-
-  // Navigate on location change.
-  useEffect(() => {
-    if (isSameLocation(location, window.location)) {
-      return;
+  const [location, setLocation] = useState(getWindowLocation);
+  const navigate = useCallback(({ pathname, replace, state }) => {
+    if (replace) {
+      window.history.replaceState({ state }, null, pathname);
+    } else {
+      window.history.pushState({ state }, null, pathname);
     }
+    setLocation(getWindowLocation());
+  }, []);
 
-    const { pathname, state } = location;
-
-    window.history.pushState({ state }, null, pathname);
-  }, [location]);
+  const contextValue = useMemo(() => ({ goBack, location, navigate }), [
+    goBack,
+    location,
+    navigate,
+  ]);
 
   // Listen to pop state event.
   useEffect(() => {
     function handlePopState() {
-      setLocation({ pathname: window.location.pathname });
+      setLocation(getWindowLocation());
     }
 
     window.addEventListener('popstate', handlePopState);
