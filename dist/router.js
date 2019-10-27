0397e61260f5c63296d8cd043d0d64aefd27eafe
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-10-27 18:18:14 +0000

    v0.2.0

--- dist/router.js
+++ dist/router.js
@@ -11,29 +11,45 @@ var _propTypes = _interopRequireDefault(require('prop-types'));
 
 var _react = _interopRequireWildcard(require('react'));
 
+function _getRequireWildcardCache() {
+  if (typeof WeakMap !== 'function') return null;
+  var cache = new WeakMap();
+  _getRequireWildcardCache = function _getRequireWildcardCache() {
+    return cache;
+  };
+  return cache;
+}
+
 function _interopRequireWildcard(obj) {
   if (obj && obj.__esModule) {
     return obj;
-  } else {
-    var newObj = {};
-    if (obj != null) {
-      for (var key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-          var desc =
-            Object.defineProperty && Object.getOwnPropertyDescriptor
-              ? Object.getOwnPropertyDescriptor(obj, key)
-              : {};
-          if (desc.get || desc.set) {
-            Object.defineProperty(newObj, key, desc);
-          } else {
-            newObj[key] = obj[key];
-          }
+  }
+  var cache = _getRequireWildcardCache();
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+  var newObj = {};
+  if (obj != null) {
+    var hasPropertyDescriptor =
+      Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for (var key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        var desc = hasPropertyDescriptor
+          ? Object.getOwnPropertyDescriptor(obj, key)
+          : null;
+        if (desc && (desc.get || desc.set)) {
+          Object.defineProperty(newObj, key, desc);
+        } else {
+          newObj[key] = obj[key];
         }
       }
     }
-    newObj['default'] = obj;
-    return newObj;
   }
+  newObj['default'] = obj;
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+  return newObj;
 }
 
 function _interopRequireDefault(obj) {
@@ -51,6 +67,14 @@ function _nonIterableRest() {
 }
 
 function _iterableToArrayLimit(arr, i) {
+  if (
+    !(
+      Symbol.iterator in Object(arr) ||
+      Object.prototype.toString.call(arr) === '[object Arguments]'
+    )
+  ) {
+    return;
+  }
   var _arr = [];
   var _n = true;
   var _d = false;
@@ -141,7 +165,7 @@ var Router = function Router(_ref) {
         navigate: navigate,
       };
     },
-    [goBack, location, navigate]
+    [location, navigate]
   ); // Listen to pop state event.
 
   (0, _react.useEffect)(function() {
